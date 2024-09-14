6672bb7c49463a081cb42d1622e8b685d0017da7
bdvil <benjam.devillers@gmail.com>
2024-09-14 18:56:41 +0200

    update PROJECT_DIR path

--- src/pyrefactorlsp/constants.py
+++ src/pyrefactorlsp/constants.py
@@ -1,11 +1,11 @@
 import logging
 from pathlib import Path
 
-PROJECT_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 LOGGING_LEVEL = logging.DEBUG
 LOGGER = logging.getLogger("pyrefactorlsp")
 
-handler = logging.FileHandler(Path(__name__).parent.parent / "pyrefactorlsp.log")
+handler = logging.FileHandler(PROJECT_DIR / "pyrefactorlsp.log")
 handler.setLevel(LOGGING_LEVEL)
 LOGGER.setLevel(LOGGING_LEVEL)
 LOGGER.addHandler(handler)
